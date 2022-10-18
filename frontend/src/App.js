025760a1c1e6a6379dc2bccc7fa29244d7687bdc
nmarkovic <pomocnih@gmail.com>
2022-10-18 16:29:59 +0200

    feat (ELN-1): render routes dynamically

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,116 +1,35 @@
-import React, { Fragment, useEffect } from 'react'
-import Home from './pages/index'
-import Login from './pages/authentication/login'
-import Admin from './pages/admin'
-import { Routes, Route } from 'react-router-dom'
-import Register from './pages/authentication/register'
-import Student from './pages/student'
-import Teacher from './pages/teacher'
-import Media from './pages/admin/media/index'
-import AddMedia from './pages/admin/media/add-media/addMedia'
-import { useLocation, Outlet } from 'react-router-dom'
-import Users from './pages/admin/users'
-import AddUser from './pages/admin/users/addUser'
-import CoursesOverview from './pages/admin/courses/courses-overview'
-import CreateCourse from './pages/admin/courses/create-course'
-import Profile from './pages/admin/users/profile'
-import EditUser from './pages/admin/users/editUser'
-import EditCourse from './pages/admin/courses/edit-course'
-import EditMedia from './pages/admin/media/edit-media'
-import CourseOverview from './pages/student/course-overview'
-import EditStudentProfile from './pages/student/edit-user'
-import { useNavigate } from 'react-router-dom'
-function App() {
-    const navigate = useNavigate()
-    const location = useLocation()
-    const userId = localStorage.getItem('userId')
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import useFetch from "./lib/useFetch";
 
-    useEffect(() => {
-        if (!userId) {
-            navigate('/')
-        }
-    }, [])
+function App({ routes }) {
+  const navigate = useNavigate();
 
-    return (
-        <Fragment>
-            <Routes>
-                <Route path="/" element={<Home />} />
+  const userId = localStorage.getItem("userId");
+  const { isLoading, isError, data } = useFetch(
+    `${process.env.REACT_APP_BACKEND_URL}:${
+      process.env.REACT_APP_BACKEND_PORT
+    }/getUser/${3}`
+  );
+  useEffect(() => {
+    if (!userId) {
+      navigate("/");
+    }
+  }, []);
 
-                {/* AUTH ROUTES */}
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-
-                {/* ADMIN ROUTES */}
-                <Route
-                    path="/admin"
-                    element={
-                        location.pathname === '/admin' ? <Admin /> : <Outlet />
-                    }>
-                    {/* ADMIN PROFILE*/}
-                    <Route path="/admin/profile/" element={<Profile />} />
-
-                    {/* MEDIA ROUTES*/}
-                    <Route
-                        path="/admin/media/media-overview"
-                        element={<Media />}
-                    />
-                    <Route
-                        path="/admin/media/add-media"
-                        element={<AddMedia />}
-                    />
-                    <Route
-                        path="/admin/courses/edit-medium/:mid"
-                        element={<EditMedia />}
-                    />
-                    {/* USER ROUTES */}
-                    <Route
-                        path="/admin/users/users-overview"
-                        element={<Users />}
-                    />
-                    <Route path="/admin/users/add-user" element={<AddUser />} />
-                    <Route
-                        path="/admin/users/edit-profile/:uid"
-                        element={<EditUser />}
-                    />
-                    {/* COURSES ROUTES*/}
-                    <Route
-                        path="/admin/courses/courses-overview"
-                        element={<CoursesOverview />}
-                    />
-
-                    <Route
-                        path="/admin/courses/add-course"
-                        element={<CreateCourse />}
-                    />
-                    <Route
-                        path="/admin/courses/edit-course/:cid"
-                        element={<EditCourse />}
-                    />
-                </Route>
-                {/* TEACHER ROUTES */}
-
-                <Route path="/teacher" element={<Teacher />} />
-
-                {/* STUDENT ROUTES */}
-
-                <Route path="/student" element={<Student />} />
-                <Route
-                    path="/student/courses/:cid"
-                    element={<CourseOverview />}
-                />
-                <Route
-                    path="/student/profile/:uid"
-                    element={<EditStudentProfile />}
-                />
-                {/*<Route index element={<Home />} />*/}
-                {/*<Route path="teams" element={<Teams />}>*/}
-                {/*    <Route path=":teamId" element={<Team />} />*/}
-                {/*    <Route path="new" element={<NewTeamForm />} />*/}
-                {/*    <Route index element={<LeagueStandings />} />*/}
-                {/*</Route>*/}
-            </Routes>
-        </Fragment>
-    )
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return <h1>Error happened</h1>;
+  }
+  return (
+    <Routes>
+      {routes.map((route) => (
+        <Route path={route.path} element={route.component} />
+      ))}
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './pages/index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { ToastProvider } from 'react-toast-notifications'
-
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./App";
+import "./pages/index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToastProvider } from "react-toast-notifications";
+import routes from "./routes";
 ReactDOM.render(
-    <BrowserRouter>
-        <ToastProvider placement="bottom-right">
-            <App />
-        </ToastProvider>
-    </BrowserRouter>,
-    document.getElementById('root'),
-)
+  <BrowserRouter>
+    <ToastProvider placement="bottom-right">
+      <App routes={routes} />
+    </ToastProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
