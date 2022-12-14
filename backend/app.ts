91270e96f01a09152b940980dd3a29618633a22d
nmarkovic <pomocnih@gmail.com>
2022-12-15 00:09:20 +0100

    feat(ELN-0): allow server to handle all requests

--- backend/app.ts
+++ backend/app.ts
@@ -1,24 +1,29 @@
-const express = require('express');
+const express = require("express");
 
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-const userRouter = require('./routes/userRoutes');
+const cors = require("cors");
+const userRouter = require("./routes/userRoutes");
 
 import { Express } from "express";
 
-const app:Express = express();
+const app: Express = express();
 
 // 1 MIDDLEWARES
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // 3 ROUTES
-app.use('/api/v1/users', userRouter);
-
+app.use("/api/v1/users", userRouter);
 
 // 4 START A SERVER
 module.exports = app;
